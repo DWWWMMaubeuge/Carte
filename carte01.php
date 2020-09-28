8eaec49f6df48ef2e95205575306afb71f5fdf0c
xavier <maubeuge.dwwm@gmail.com>
2020-09-28 10:26:39 +0200

    init projet 0.0

---
+++ carte01.php
@@ -0,0 +1,31 @@
+<?php
+
+class Carte
+{
+	public $couleur;
+	public $valeur;
+
+	public function __construct(  $col, $val )
+	{
+
+	} __
+
+	public function show( )
+	{
+
+	} __
+}
+
+
+$c1 = new Carte( "Pique", "As");
+
+$c1.show();
+// As de Pique
+
+
+
+
+
+
+
+?>
