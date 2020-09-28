f2db8e16f5f65efd2be429846c87b5a75c8005c4
redlafougere <69895192+redlafougere@users.noreply.github.com>
2020-09-28 11:27:30 +0200

    Update carte01.php

--- carte01.php
+++ carte01.php
@@ -7,12 +7,40 @@ class Carte
 
 	public function __construct(  $col, $val )
 	{
+		$this->couleur = $col;
+		$this->valeur = $val;
 
-	} __
+        $card_col = array(
+            'H' => 'Coeur',
+            'S' => 'Pique',
+            'D' => 'Carreau',
+            'C' => 'Trefle'
+        );
+        $card_val = array(
+            'A' => 'As',
+            '2' => 'Deux',
+            '3' => 'Trois',
+            '4' => 'Quatre',
+            '5' => 'Cinq',
+            '6' => 'Six',
+            '7' => 'Sept',
+            '8' => 'Huit',
+            '9' => 'Neuf',
+            '10' => 'Dix',
+            'V' => 'Valet',
+            'Q' => 'Reine',
+            'K' => 'Roi'
+        );
+        $this->col = $col;
+        $this->val = $val;
+        $this->col_text = $card_col[$col];
+        $this->val_text = $card_val[$val];
+    }
 
 	public function show( )
 	{
-
+		echo "<p>".$this->couleur."</p>\n";
+		echo "<p>".$this->valeur."</p>\n";
 	} __
 }
 
