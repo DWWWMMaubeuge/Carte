48a8b4c40505e05fdf4ed85367b6e499d48898d3
redlafougere <69895192+redlafougere@users.noreply.github.com>
2020-09-28 11:29:36 +0200

    Update carte01.php

--- carte01.php
+++ carte01.php
@@ -1,5 +1,8 @@
 <?php
 
+$cards_valeur = array( 'as', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix', 'valet', 'cavalier', 'dame', 'roi' );
+$cards_couleur = array( 'carreau' , 'pique', 'coeur', 'trefle' );
+
 class Carte
 {
 	public $couleur;
@@ -50,8 +53,25 @@ $c1 = new Carte( "Pique", "As");
 $c1.show();
 // As de Pique
 
+class Jeu
+{
+	public $cartes = array();
 
 
+	public function __construct(  )
+	{
+		GLOBAL $cards_couleur, $cards_valeur;
+		
+		foreach( $cards_couleur as $couleur ) 
+			foreach ($cards_valeur as $valeur) 
+			{
+				Carte( $couleur, $valeur );
+			}
+	}
+
+
+
+}
 
 
 
