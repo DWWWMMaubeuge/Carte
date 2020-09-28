62d7cd45fb2434bd1cecb53a9caa649f9f779000
Maxime <maximewilmot@gmail.com>
2020-09-28 11:03:12 +0200

    mes cartes

--- carte01.php
+++ carte01.php
@@ -4,22 +4,26 @@ class Carte
 {
 	public $couleur;
 	public $valeur;
+	public $image;
 
-	public function __construct(  $col, $val )
+	public function __construct(  $col, $val, $img )
 	{
-
-	} __
+		$this->couleur = $col;
+		$this->valeur  = $val;
+		$this->image  = $img;
+	} 
 
 	public function show( )
 	{
-
-	} __
+		echo "<h1>".$this->couleur."</h1>\n";	
+		echo "<h1>".$this->valeur."</h1>\n";
+		echo "<img src='" .$this->image."'  width='150' height='150' >" ;
+}
 }
 
+$c1 = new Carte( "Pique", "As","<imgsrc>");
 
-$c1 = new Carte( "Pique", "As");
-
-$c1.show();
+$c1->show();
 // As de Pique
 
 
