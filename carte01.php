7f59ee08feef4c62e3c2ece7d3d74ae05cbc3e88
Maxime <maximewilmot@gmail.com>
2020-09-28 11:34:32 +0200

    11h20

--- carte01.php
+++ carte01.php
@@ -14,14 +14,13 @@ class Carte
 	} 
 
 	public function show( )
-	{
-		echo "<h1>".$this->couleur."</h1>\n";	
-		echo "<h1>".$this->valeur."</h1>\n";
-		echo "<img src='" .$this->image."'  width='150' height='150' >" ;
+	{	
+		echo "<p>".$this->valeur." de ".$this->couleur."</p>\n";	
+		//echo "<img src=>'" .$this->image."'  width='150' height='150' >" ;
 }
 }
 
-$c1 = new Carte( "Pique", "As","<imgsrc>");
+$c1 = new Carte( "Pique", "As","<img src= Photos-01.jpg>");
 
 $c1->show();
 // As de Pique
