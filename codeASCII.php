a02448d04e2f12cc53ba6013a41034be1778d1ae
59012-30-05 <59012-30-05@exchange.ad.afpanet>
2020-09-29 09:56:55 +0200

    save

--- codeASCII.php
+++ codeASCII.php
@@ -1,27 +1,14 @@
 <?php
 
-
-
 $lettre = 'A';
 echo ord( $lettre );
 
 echo "<br>";
-
 
 for ($code = 65;  $code < 128 ; $code++ )
 	echo chr( $code );
 
 for ($code = ord( 'A' );  $code < ord( 'Z' ) ; $code++ )
 	echo chr( $code );
-
-
-
-
-
-
-
-
-
-
 
 ?>
