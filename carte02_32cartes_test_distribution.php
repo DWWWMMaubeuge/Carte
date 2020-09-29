d15b0e1c1631e1947a75c8092e394e89b5c7c152
xavier <maubeuge.dwwm@gmail.com>
2020-09-29 13:34:00 +0200

    corriger la distribution

---
+++ carte02_32cartes_test_distribution.php
@@ -0,0 +1,31 @@
+<?php
+
+$__TEST = false;
+
+include_once( "CA_fonctions_generales.php");
+include_once( "CA_Carte.php");
+include_once( "CA_Jeu.php");
+
+
+setHeaderNoCache();
+
+$jeu1 = new Jeu();
+$jeu1->init32();
+
+//echo "=== show en 4 lignes de 8  ===<br>";
+echo "=== show de 32  ===<br>";
+$jeu1->show();
+//$jeu1->show();
+
+echo "=== distrib 40  ===<br>";
+$nouveauJeu1 = $jeu1->distribuer( 40 );
+
+echo "============ Le nouveau jeu  ===================<br>";
+
+$nouveauJeu1->showNL(4);
+
+echo "========= Rest de carte dans le jeux =========<br>";
+$jeu1->show();
+
+
+?>
